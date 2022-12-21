0c13bab01f86c3c2a32e6194d19e139e3f6cc2e6
Mozz-Academy <59390842+Mozz-Academy@users.noreply.github.com>
2022-12-21 21:19:22 +0200

    New rewrite

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,19 @@ fn main() {
     
     let args: Vec<String> = std::env::args().collect();
     let path = &args[1];
-    let file = std::fs::read_to_string(path).expect("Failed");
-    let single_key = get_single_line_comment(get_extension(&path));
-    let open_key = get_open_multi_line_comment(get_extension(&path));
-    let closed_key = get_closed_multi_line_comment(get_extension(&path));
-    let max_len = std::cmp::max(std::cmp::max(open_key.len(), closed_key.len()), single_key.len());
+    let file = std::fs::read_to_string(&path).expect("Failed");
+    let extension = get_extension(&path);
     let mut new_file_content = String::new();
     let mut com_file_content = String::new();
-    let mut in_string = false;
-    let mut in_comment = false;
     
+    println!("{}", extension);
     
-    for l in file.split('\n') {
-        let mut curr_file_line = String::new();
-        let tup = process_line(l, &mut com_file_content, &mut curr_file_line, max_len, in_comment, in_string, single_key, open_key, closed_key);
-        in_comment = tup.0;
-        in_string = tup.1;
-        if !curr_file_line.trim().is_empty() {
-            new_file_content.push_str(&curr_file_line);
-            new_file_content.push('\n');
-        }
+    for l in file.split("\r\n") {
+        let tup = process_line(l, extension);
+        println!("{:?}", tup);
+        write_to(&mut new_file_content, &tup.0.unwrap());
+        write_to(&mut com_file_content, &tup.1.unwrap());
     }
-
-
-    // println!("{}", com_file_content);
-    // println!("--------------------");
-    // println!("{}", new_file_content);
 
     std::fs::write(path, new_file_content).expect("Failed");
     std::fs::write(path.to_owned() + ".comm", com_file_content).expect("Failed");
--- src/utils.rs
+++ src/utils.rs
@@ -6,21 +6,21 @@ pub fn get_single_line_comment(ext: &str) -> &str {
     }
 }
 
-pub fn get_open_multi_line_comment(ext: &str) -> &str {
-    match ext {
-        "rs" | "c" | "cpp" | "java" | "js" | "ts" | "go" | "fs" | "fsx" | "cs" | "h" | "hpp" => "/*",
-        _ => panic!(".{} is not yet implemented", ext)
-    }
-}
+// pub fn get_open_multi_line_comment(ext: &str) -> &str {
+//     match ext {
+//         "rs" | "c" | "cpp" | "java" | "js" | "ts" | "go" | "fs" | "fsx" | "cs" | "h" | "hpp" => "/*",
+//         _ => panic!(".{} is not yet implemented", ext)
+//     }
+// }
 
-pub fn get_closed_multi_line_comment(ext: &str) -> &str {
-    match ext {
-        "rs" | "c" | "cpp" | "java" | "js" | "ts" | "go" | "fs" | "fsx" | "cs" | "h" | "hpp" => "*/",
-        _ => panic!(".{} is not yet implemented", ext)
-    }
-}
+// pub fn get_closed_multi_line_comment(ext: &str) -> &str {
+//     match ext {
+//         "rs" | "c" | "cpp" | "java" | "js" | "ts" | "go" | "fs" | "fsx" | "cs" | "h" | "hpp" => "*/",
+//         _ => panic!(".{} is not yet implemented", ext)
+//     }
+// }
  
-pub fn find_dot(src: &str) -> usize {
+pub fn find_last_dot(src: &str) -> usize {
     let mut i = 0usize;
     let mut idx = 0usize;
     for ch in src.chars() {
@@ -33,7 +33,7 @@ pub fn find_dot(src: &str) -> usize {
 }
 
 pub fn get_extension(src: &str) -> &str {
-    &src[find_dot(src)+1..src.len()]
+    &src[find_last_dot(src)+1..src.len()]
 }
 
 pub fn write_to(src: &mut String, to_write: &str) {
@@ -41,69 +41,46 @@ pub fn write_to(src: &mut String, to_write: &str) {
     src.push('\n');
 }
 
-pub fn process_line(l: &str, com: &mut String, nl: &mut String, max: usize, mut in_comment: bool, mut in_string: bool, single_key: &str, open_key: &str, closed_key: &str) -> (bool, bool) {
-    let line = l.trim();
-    if !line.is_empty() && line.len() >= max {
-        // in_string
+pub fn process_line(line: &str, ext: &str) -> (Option<String>, Option<String>) {
+    let sgtok = get_single_line_comment(ext);
+    let mut matched = true;
+    let mut fq = 0usize;
+    let tok_zones: Vec<usize> = line.match_indices(sgtok).map(|(i, _)|i).collect();
+    let mut string_zones: Vec<(usize, usize)> = Vec::new();
+    let mut found_sgtok = false;
+    let mut sgtok_position = 0usize; // Default value
 
-        if !in_comment {
-            if !in_string {
-                if line.contains(single_key) && !l.contains(open_key) {
-                    write_to(nl, &l[0..l.find(single_key).unwrap()]);
-                    write_to(com, &l[l.find(single_key).unwrap()..l.len()]);
-                    return (in_comment, in_string);
-                }
-                else if l.contains(single_key) && l.contains(open_key) {
-                    if l.find(open_key).unwrap() < l.find(single_key).unwrap() {
-                        in_comment = true;
+    for (i, ch) in line.chars().enumerate() {
+        if ch == '\"' {
+            if i > 0 {
+                if line.chars().nth(i).unwrap() != '\\' {
+                    matched = !matched;
+                    match matched {
+                        false => fq = i,
+                        true => {
+                            string_zones.push((fq, i));
+                            fq = 0usize;
+                        }
                     }
-                    else {
-                        write_to(nl, &l[0..l.find(single_key).unwrap()]);
-                        write_to(com, &l[l.find(single_key).unwrap()..l.len()]);
-                    }
-                }
-                else if !l.contains(single_key) && l.contains(open_key) {
-                    in_comment = true;
-                    write_to(nl, &l[0..l.find(open_key).unwrap()]);
-                    write_to(com, &l[l.find(open_key).unwrap()..l.len()]);
-                }
-                else if l.contains(open_key) && l.contains(closed_key) {
-                    write_to(nl, &l[0..l.find(open_key).unwrap()]);
-                    write_to(com, &l[l.find(open_key).unwrap()..l.find(closed_key).unwrap()+closed_key.len()]);
-                    write_to(nl, &l[l.find(closed_key).unwrap()+closed_key.len()..l.len()]);
-                }
-                else {
-                    nl.push_str(&l);
                 }
             }
-        }
-        else {
-            if !in_string {
-                if l.contains(closed_key) && !l.contains(single_key) {
-                    write_to(com, &l[0..l.find(closed_key).unwrap()+closed_key.len()]);
-                    let tup = process_line(&l[l.find(closed_key).unwrap()+closed_key.len()..l.len()], com, nl, max, false, in_string, single_key, open_key, closed_key);
-                    in_comment = tup.0;
-                    in_string = tup.1;
-                }
-                else if l.contains(closed_key) && l.contains(single_key) {
-                    if l.find(closed_key).unwrap() < l.find(single_key).unwrap() {
-                        in_comment = false;
-                        write_to(com, &l[0..l.find(closed_key).unwrap()+closed_key.len()]);
-                    }
-                }
-                else {
-                    write_to(com, &l);
-                }
+            else {
+                matched = false;
             }
         }
     }
-    else if !line.is_empty() {
-        if !in_comment {
-            write_to(nl, &l);
-        }
-        else {
-            write_to(com, &l);
+
+    for tzone in &tok_zones {
+        for szones in &string_zones {
+            if *tzone < szones.0 {
+                found_sgtok = true;
+                sgtok_position = *tzone;
+                break;
+            }
         }
     }
-    (in_comment, in_string)
+    if !found_sgtok {
+        return (Some("".to_string()), Some(line.to_string()));
+    }
+    (Some(line[0..sgtok_position].to_string()), Some(line[sgtok_position+sgtok.len()..line.len()].to_string()))
 }
